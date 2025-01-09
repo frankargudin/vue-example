da826be70007764a29cd671c615e58acc61f21e0
fargudin <frankargudin@gmail.com>
2025-01-09 14:03:16 -0500

    Create component ContadorComponent

---
+++ src/components/ContadorComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ count }}</p>
+  <button @click="incrementarCount">Incrementar</button>
+  <button @click="decrementarCount">Decrementar</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const incrementarCount = () => {
+  count.value++
+}
+const decrementarCount = () => {
+  count.value--
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
